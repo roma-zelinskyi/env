4dccfb9c180aecc5b047b49741a4d0af35e44dd8
Roman Zelinskyi <rzelinskyi@luxoft.com>
2019-11-28 12:03:37 +0100

    feat: Add OS and DIST detection
    Added support to setup env for Debian
    based distribution.
    
    Signed-off-by: Roman Zelinskyi <lord.zelinskyi@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+export OS=`uname -s`
+
+if [ "${OS}" = "Linux" ] ; then
+	if [ -f /etc/lsb-release ] ; then
+		export DIST=`lsb_release -si`
+        export RELEASE=`lsb_release -sr`
+    else
+        echo "Unsupported DISTRO!"
+        exit
+	fi
+else
+    echo "Unsupported OS!"
+    exit
+fi
+
+echo "${OS} ${DIST}(${RELEASE})"
+
 ./neovim/setup.sh
